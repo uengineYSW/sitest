15d21e203d27296aea4b5bd9bc07195c5bd1e8cf
uengineYSW <112850933+uengineYSW@users.noreply.github.com>
2024-08-08 16:07:00 +0900

    Bug fix: Code.java 파일의 잘못된 구문으로 인해 컴파일 오류가 발생했습니다. 파일 끝에 도달하기 전에 구문 분석이 완료되지 않았습니다. 따라서 제대로 된 클래스 정의를 추가하여 이 문제를 해결했습니다.

--- CodeService/src/main/java/sitest/domain/Code.java
+++ CodeService/src/main/java/sitest/domain/Code.java
@@ -1,1 +1,124 @@
-package sitest
+package sitest;
+
+public class Code {
+
+    private Long id;
+    private String itemCode;
+    private String codeNo;
+    private String code;
+    private String codeName;
+    private String isSys;
+    private String isUse;
+    private String etc;
+    private String etc1;
+    private String etc2;
+    private String etc3;
+    private String etc4;
+    private String etc5;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getCodeNo() {
+        return codeNo;
+    }
+
+    public void setCodeNo(String codeNo) {
+        this.codeNo = codeNo;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCodeName() {
+        return codeName;
+    }
+
+    public void setCodeName(String codeName) {
+        this.codeName = codeName;
+    }
+
+    public String getIsSys() {
+        return isSys;
+    }
+
+    public void setIsSys(String isSys) {
+        this.isSys = isSys;
+    }
+
+    public String getIsUse() {
+        return isUse;
+    }
+
+    public void setIsUse(String isUse) {
+        this.isUse = isUse;
+    }
+
+    public String getEtc() {
+        return etc;
+    }
+
+    public void setEtc(String etc) {
+        this.etc = etc;
+    }
+
+    public String getEtc1() {
+        return etc1;
+    }
+
+    public void setEtc1(String etc1) {
+        this.etc1 = etc1;
+    }
+
+    public String getEtc2() {
+        return etc2;
+    }
+
+    public void setEtc2(String etc2) {
+        this.etc2 = etc2;
+    }
+
+    public String getEtc3() {
+        return etc3;
+    }
+
+    public void setEtc3(String etc3) {
+        this.etc3 = etc3;
+    }
+
+    public String getEtc4() {
+        return etc4;
+    }
+
+    public void setEtc4(String etc4) {
+        this.etc4 = etc4;
+    }
+
+    public String getEtc5() {
+        return etc5;
+    }
+
+    public void setEtc5(String etc5) {
+        this.etc5 = etc5;
+    }
+}
